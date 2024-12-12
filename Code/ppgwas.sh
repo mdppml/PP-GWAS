0f00eabec9a1aa5d033a26b1b7ea5aacde1ccfb7
Arjhun Swaminathan <114734554+Arjhun-alt@users.noreply.github.com>
2024-12-12 17:53:31 +0100

    Update ppgwas.sh

--- Code/ppgwas.sh
+++ Code/ppgwas.sh
@@ -27,7 +27,7 @@ sleep 4
 
 for i in $(seq 1 $7)
 do
-  srun --exclusive -u --nodes=1 --ntasks=1 run_client.sh $1 $2 $3 $4 $5 $6 $7 $i $8 $client_DIR & 
+  srun --exclusive -u --nodes=1 --ntasks=1 run_client.sh $1 $2 $3 $4 $5 $6 $7 $8 $i $client_DIR & 
   sleep 0.1
 done
 
