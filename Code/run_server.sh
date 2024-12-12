53e15c419f3ca3b0378f64cd38bdecc1a07cce4a
Arjhun Swaminathan <114734554+Arjhun-alt@users.noreply.github.com>
2024-12-12 15:45:15 +0100

    Update run_server.sh

--- Code/run_server.sh
+++ Code/run_server.sh
@@ -4,6 +4,6 @@ echo "Starting job script for server"
 
 eval "$(conda shell.bash hook)"
 echo "Activating conda environment for server"
-conda activate /home/swaminathan/miniconda3/envs/ppregenie_env
+conda activate "INSERT_CONDA_ENVIRONMENT_HERE"
 
 python -u server.py --number_of_clients $7 --base_port $1 --number_of_samples $2 --number_of_snps $3 --number_of_covariates $4 --number_of_blocks $5 --number_of_folds $6 --number_of_blocks_per_run $8
