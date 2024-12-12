3825d40168f29b52da806a04b1afe7b0c9aecad8
Arjhun Swaminathan <114734554+Arjhun-alt@users.noreply.github.com>
2024-12-12 15:44:37 +0100

    Update run_client.sh

--- Code/run_client.sh
+++ Code/run_client.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-client_id=$1
+client_id=$8
+DIR=${10}
 
-echo "Starting job script for client $8"
+mkdir -p $DIR
 
-eval "$(conda shell.bash hook)"
-echo "Activating conda environment for client $8"
-conda activate /home/swaminathan/miniconda3/envs/ppregenie_env
+client_dir="${DIR}/client${client_id}"
+mkdir -p $client_dir
 
-python -u client.py --number_of_clients $7 --party_id $8 --base_port $1 --number_of_samples $2 --number_of_snps $3 --number_of_covariates $4 --number_of_blocks $5 --number_of_folds $6 --number_of_blocks_per_run $9
+echo "Starting job script for client $client_id" > ${client_dir}/output.txt 2> ${client_dir}/error.txt
+
+eval "$(conda shell.bash hook)" >> ${client_dir}/output.txt 2>> ${client_dir}/error.txt
+echo "Activating conda environment for client $client_id" >> ${client_dir}/output.txt 2>> ${client_dir}/error.txt
+conda activate "INSERT_CONDA_ENVIRONMENT_HERE" >> ${client_dir}/output.txt 2>> ${client_dir}/error.txt
+
+python -u client.py --number_of_clients $7 --party_id $8 --base_port $1 --number_of_samples $2 --number_of_snps $3 --number_of_covariates $4 --number_of_blocks $5 --number_of_folds $6 --number_of_blocks_per_run $9 >> ${client_dir}/output.txt 2>> ${client_dir}/error.txt
