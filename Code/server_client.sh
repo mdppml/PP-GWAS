6dd8542a783912e31a3b7612487893cf234a2e6e
Arjhun Swaminathan <114734554+Arjhun-alt@users.noreply.github.com>
2024-12-12 15:43:48 +0100

    Update server_client.sh

--- Code/server_client.sh
+++ Code/server_client.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
-#SBATCH --job-name=PPF-GWAS
-#SBATCH --nodes=7
-#SBATCH --ntasks=7
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=128G 
-#SBATCH --time=03:00:00
+#SBATCH --job-name=ppREGENIE
+#SBATCH --nodes=4  # One for the server and one for each client
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=24
+#SBATCH --time=03:59:00
 #SBATCH --partition=day
-#SBATCH --output=job_output_%j.txt
-rm -f /home/swaminathan/ppREGENIE/Data/server_ready_*.txt
-rm /home/swaminathan/ppREGENIE/Data/ip_address_file.txt
+#SBATCH --mem=240000
+#SBATCH --exclude=tcml-node33
+#SBATCH --output=test_%j.txt
+# Define the log folder with the job ID
+client_DIR="logs/job_$SLURM_JOB_ID/"
+server_DIR="logs/job_$SLURM_JOB_ID/server"
+mkdir -p $client_DIR
+mkdir -p $server_DIR
+
+rm -f ../test_site/Data/server_ready_*.txt
+rm ../test_site/Data/ip_address_file.txt
 chmod +x run_server.sh
 chmod +x run_client.sh
 
 export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-srun --exclusive -u --nodes=1 --ntasks=1 run_server.sh $1 $2 $3 $4 $5 $6 $7 $8 &
+srun --exclusive -u --nodes=1 --ntasks=1 run_server.sh $1 $2 $3 $4 $5 $6 $7 $8 > $server_DIR/output.txt 2> $server_DIR/error.txt &
 
 sleep 4
 
 for i in $(seq 1 $7)
 do
-  srun --exclusive -u --nodes=1 --ntasks=1 run_client.sh $1 $2 $3 $4 $5 $6 $7 $i $8 &
-  sleep 0.15
+  srun --exclusive -u --nodes=1 --ntasks=1 run_client.sh $1 $2 $3 $4 $5 $6 $7 $i $8 $client_DIR & 
+  sleep 0.1
 done
 
 wait
 
 echo "Job script finished"
-
