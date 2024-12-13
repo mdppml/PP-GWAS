07574bc06101acddf2942454dba14a7c006fee11
Arjhun Swaminathan <114734554+Arjhun-alt@users.noreply.github.com>
2024-12-13 11:33:00 +0100

    Update run_server.sh

--- Code/run_server.sh
+++ Code/run_server.sh
@@ -6,4 +6,4 @@ eval "$(conda shell.bash hook)"
 echo "Activating conda environment for server"
 conda activate "INSERT_CONDA_ENVIRONMENT_HERE"
 
-python -u server.py --number_of_clients $7 --base_port $1 --number_of_samples $2 --number_of_snps $3 --number_of_covariates $4 --number_of_blocks $5 --number_of_folds $6 --number_of_blocks_per_run $8
+python -u server.py --number_of_parties $7 --base_port $1 --number_of_samples $2 --number_of_snps $3 --number_of_covariates $4 --number_of_blocks $5 --number_of_folds $6 --number_of_blocks_per_run $8
