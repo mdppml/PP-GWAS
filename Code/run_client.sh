4905269ce109904507d9f03953be77df1659444f
Arjhun Swaminathan <114734554+Arjhun-alt@users.noreply.github.com>
2024-12-13 11:33:45 +0100

    Update run_client.sh

--- Code/run_client.sh
+++ Code/run_client.sh
@@ -14,4 +14,4 @@ eval "$(conda shell.bash hook)" >> ${party_dir}/output.txt 2>> ${party_dir}/erro
 echo "Activating conda environment for party $party_id" >> ${party_dir}/output.txt 2>> ${party_dir}/error.txt
 conda activate "INSERT_CONDA_ENVIRONMENT_HERE" >> ${party_dir}/output.txt 2>> ${party_dir}/error.txt
 
-python -u party.py --number_of_parties $7 --party_id $9 --base_port $1 --number_of_samples $2 --number_of_snps $3 --number_of_covariates $4 --number_of_blocks $5 --number_of_folds $6 --number_of_blocks_per_run $8 >> ${party_dir}/output.txt 2>> ${party_dir}/error.txt
+python -u client.py --number_of_parties $7 --party_id $9 --base_port $1 --number_of_samples $2 --number_of_snps $3 --number_of_covariates $4 --number_of_blocks $5 --number_of_folds $6 --number_of_blocks_per_run $8 >> ${party_dir}/output.txt 2>> ${party_dir}/error.txt
