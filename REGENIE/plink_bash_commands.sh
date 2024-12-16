d45fd74ccce0925bed21b4d484c496c05b62eea3
Arjhun Swaminathan <114734554+Arjhun-alt@users.noreply.github.com>
2024-12-16 15:16:57 +0100

    Update plink_bash_commands.sh

--- REGENIE/plink_bash_commands.sh
+++ REGENIE/plink_bash_commands.sh
@@ -1,2 +1,2 @@
-./plink --file /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/output --make-bed --out /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/output
-./plink2-2 --bfile /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/output --export bgen-1.2 --out /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/output
+./plink --file /output --make-bed --out /output
+./plink2-2 --bfile /output --export bgen-1.2 --out /output
