a84501d9495fb3d3d27eb14c02a2cbb74a782ac0
Arjhun Swaminathan <114734554+Arjhun-alt@users.noreply.github.com>
2024-12-16 15:19:09 +0100

    Update regenie_bash_commands.sh

--- REGENIE/regenie_bash_commands.sh
+++ REGENIE/regenie_bash_commands.sh
@@ -1,24 +1,24 @@
 export MKL_DEBUG_CPU_TYPE=5
 ./regenie \
 --step 1 \
---bed /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/output \
---covarFile /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/covariates.txt \
---phenoFile /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/phenotype.txt \
+--bed ../test_site/N27895_M516348_C2_P6_B126/REGENIE/output \
+--covarFile ../test_site/N27895_M516348_C2_P6_B126/REGENIE/covariates.txt \
+--phenoFile ../test_site/N27895_M516348_C2_P6_B126/REGENIE/phenotype.txt \
 --bsize 4098 \
 --force-qt --lowmem \
 --cv 5 \
 --lowmem-prefix tmp_rg \
 --ignore-pred \
---out /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/fit_bin_out
+--out ../test_site/N27895_M516348_C2_P6_B126/REGENIE/fit_bin_out
 
 ./regenie \
 --step 2 \
---bgen /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/output.bgen \
---covarFile /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/covariates.txt \
---phenoFile /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/phenotype.txt \
+--bgen ../test_site/N27895_M516348_C2_P6_B126/REGENIE/output.bgen \
+--covarFile ../test_site/N27895_M516348_C2_P6_B126/REGENIE/covariates.txt \
+--phenoFile ../test_site/N27895_M516348_C2_P6_B126/REGENIE/phenotype.txt \
 
 --bsize 4098 --force-qt \
 --cv 5 \
---pred /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/fit_bin_out_pred.list \
+--pred ../test_site/N27895_M516348_C2_P6_B126/REGENIE/fit_bin_out_pred.list \
 --ignore-pred \
---out /Users/arjhunswaminathan/Documents/N27895_M516348_C2_P6_B126/REGENIE/test_bin_out_step_2
+--out ../test_site/N27895_M516348_C2_P6_B126/REGENIE/test_bin_out_step_2
