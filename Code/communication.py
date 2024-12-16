c4bf46e39abfe953ef77fe066f861fe911d3b54f
Arjhun Swaminathan <114734554+Arjhun-alt@users.noreply.github.com>
2024-12-16 12:27:13 +0100

    Update communication.py

--- Code/communication.py
+++ Code/communication.py
@@ -14,34 +14,34 @@ CHUNK_SIZE = 60000
 #--------------------------------------------SENDING DATA-------------------------------------------#
 #####################################################################################################
 
-def send_data_to_server(client_socket, matrix, client_id):
-    data = serialize_matrix_client(client_id, matrix)
+def send_data_to_server(party_socket, matrix, party_id):
+    data = serialize_matrix_party(party_id, matrix)
     data_size = struct.pack('!Q', len(data))
-    client_socket.sendall(data_size)
+    party_socket.sendall(data_size)
     max_chunk_size = 8192
     total_sent = 0
     while total_sent < len(data):
         end = min(total_sent + max_chunk_size, len(data))
         chunk = data[total_sent:end]
-        client_socket.sendall(chunk)
+        party_socket.sendall(chunk)
         total_sent += len(chunk)
 
-def send_data_to_client(client_socket, matrix):
+def send_data_to_party(party_socket, matrix):
     data = serialize_matrix_server(matrix)
     data_size = struct.pack('!Q', len(data))
-    client_socket.sendall(data_size)
+    party_socket.sendall(data_size)
     chunk_count = 0
     for i in range(0, len(data), CHUNK_SIZE):
         chunk = data[i:i + CHUNK_SIZE]
-        client_socket.sendall(chunk)
+        party_socket.sendall(chunk)
         chunk_count += len(chunk)
 
-def send_data_to_clients(client_sockets,matrices):
+def send_data_to_parties(party_sockets,matrices):
     mkl.set_num_threads(4)
-    with ThreadPoolExecutor(max_workers=len(client_sockets)) as executor:
+    with ThreadPoolExecutor(max_workers=len(party_sockets)) as executor:
         futures = []
-        for p in range(len(client_sockets)):
-            futures.append(executor.submit(send_data_to_client, client_sockets[p], matrices[p]))
+        for p in range(len(party_sockets)):
+            futures.append(executor.submit(send_data_to_party, party_sockets[p], matrices[p]))
         for future in futures:
             future.result()
     mkl.set_num_threads(16)
@@ -50,7 +50,7 @@ def send_data_to_clients(client_sockets,matrices):
 #-------------------------------------------SERIALIZATION-------------------------------------------#
 #####################################################################################################
 
-def serialize_matrix_client(client_id, matrix):
+def serialize_matrix_party(party_id, matrix):
     def serialize_1d_array(array):
         return array.astype(np.float32).tobytes()
     if isinstance(matrix, np.ndarray):
@@ -69,11 +69,11 @@ def serialize_matrix_client(client_id, matrix):
     elif isinstance(matrix, list) and all(isinstance(x, np.ndarray) and x.ndim == 1 for x in matrix):
         rows, cols = len(matrix), 0
         matrix_type = 6
-        serialized_matrices = [serialize_matrix_client(client_id, mat) for mat in matrix]
+        serialized_matrices = [serialize_matrix_party(party_id, mat) for mat in matrix]
         matrix_data = b''.join(serialized_matrices)
     else:
         raise ValueError("Unsupported matrix type or invalid list elements.")
-    header = struct.pack('!4I1Q', client_id, rows, cols, matrix_type, len(matrix_data))
+    header = struct.pack('!4I1Q', party_id, rows, cols, matrix_type, len(matrix_data))
     return header + matrix_data
 
 def serialize_matrix_server(matrix):
@@ -84,18 +84,18 @@ def serialize_matrix_server(matrix):
 #####################################################################################################
 
 
-def receive_data(client_socket):
+def receive_data(party_socket):
     tt=time.time()
-    data_size_bytes = client_socket.recv(8)
+    data_size_bytes = party_socket.recv(8)
     if len(data_size_bytes) != 8:
-        raise ConnectionError("Client disconnected prematurely while sending data size.")
+        raise ConnectionError("party disconnected prematurely while sending data size.")
     tt=time.time()
     data_size = struct.unpack('!Q', data_size_bytes)[0]
     tt=time.time()
     chunks = []
     received_size = 0
     while received_size < data_size:
-        chunk = client_socket.recv(min(16384, data_size - received_size))
+        chunk = party_socket.recv(min(16384, data_size - received_size))
         if not chunk:
             break
         chunks.append(chunk)
@@ -105,16 +105,16 @@ def receive_data(client_socket):
     data = b''.join(chunks)
     return data
 
-def receive_and_sum(client_sockets):
-    def threaded_receive_data(client_socket, results, index):
-        results[index] = receive_data(client_socket)
+def receive_and_sum(party_sockets):
+    def threaded_receive_data(party_socket, results, index):
+        results[index] = receive_data(party_socket)
 
     mkl.set_num_threads(1)
-    raw_data = [None] * len(client_sockets)
+    raw_data = [None] * len(party_sockets)
     threads = []
 
-    for i, client_socket in enumerate(client_sockets):
-        thread = Thread(target=threaded_receive_data, args=(client_socket, raw_data, i))
+    for i, party_socket in enumerate(party_sockets):
+        thread = Thread(target=threaded_receive_data, args=(party_socket, raw_data, i))
         thread.start()
         threads.append(thread)
 
@@ -125,23 +125,23 @@ def receive_and_sum(client_sockets):
     masked_X = [None] * len(raw_data)
 
     for data in raw_data:
-        client_id, matrix = deserialize_matrix_server(data)
-        masked_X[client_id - 1] = matrix
+        party_id, matrix = deserialize_matrix_server(data)
+        masked_X[party_id - 1] = matrix
     del raw_data
     total_X_matrix = np.sum(masked_X, axis=0)
     del masked_X
     return total_X_matrix
 
-def receive_sum_and_append(client_sockets):
-    def threaded_receive_data(client_socket, results, index):
-        results[index] = receive_data(client_socket)
+def receive_sum_and_append(party_sockets):
+    def threaded_receive_data(party_socket, results, index):
+        results[index] = receive_data(party_socket)
 
     mkl.set_num_threads(1)
-    raw_data = [None] * len(client_sockets)
+    raw_data = [None] * len(party_sockets)
     threads = []
 
-    for i, client_socket in enumerate(client_sockets):
-        thread = Thread(target=threaded_receive_data, args=(client_socket, raw_data, i))
+    for i, party_socket in enumerate(party_sockets):
+        thread = Thread(target=threaded_receive_data, args=(party_socket, raw_data, i))
         thread.start()
         threads.append(thread)
 
@@ -152,8 +152,8 @@ def receive_sum_and_append(client_sockets):
     masked_X = [None] * len(raw_data)
 
     for data in raw_data:
-        client_id, matrix = deserialize_matrix_server(data)
-        masked_X[client_id - 1] = matrix
+        party_id, matrix = deserialize_matrix_server(data)
+        masked_X[party_id - 1] = matrix
     del raw_data
     total_X_matrix = np.sum(masked_X, axis=0)
     return total_X_matrix, masked_X
@@ -167,7 +167,7 @@ def receive_data_from_server(server_socket):
         if not chunk:
             raise ConnectionError("Connection closed before all data was received.")
         received_data.extend(chunk)
-    matrix = deserialize_matrix_client(received_data)
+    matrix = deserialize_matrix_party(received_data)
     return matrix
 
 #####################################################################################################
@@ -176,7 +176,7 @@ def receive_data_from_server(server_socket):
 
 def deserialize_matrix_server(serialized_data):
     header_length = 4 * 4 + 1 * 8
-    client_id, rows, cols, matrix_type, data_length = struct.unpack('!4I1Q', serialized_data[:header_length])
+    party_id, rows, cols, matrix_type, data_length = struct.unpack('!4I1Q', serialized_data[:header_length])
     matrix_data = serialized_data[header_length:]
     del serialized_data
     if matrix_type == 0:
@@ -191,7 +191,7 @@ def deserialize_matrix_server(serialized_data):
         matrix = []
         offset = 0
         for _ in range(rows):
-            sub_client_id, sub_rows, _, sub_matrix_type, sub_data_length = struct.unpack(
+            sub_party_id, sub_rows, _, sub_matrix_type, sub_data_length = struct.unpack(
                 '!4I1Q', matrix_data[offset:offset + header_length]
             )
             offset += header_length
@@ -200,7 +200,7 @@ def deserialize_matrix_server(serialized_data):
             matrix.append(np.frombuffer(sub_matrix_data, dtype=np.float32))
     else:
         raise ValueError("Unsupported matrix type.")
-    return client_id, matrix
+    return party_id, matrix
 
-def deserialize_matrix_client(data):
+def deserialize_matrix_party(data):
     return pickle.loads(data)
