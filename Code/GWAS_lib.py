94f777acb582a0632e376bd0033dc53b71b4544a
Arjhun Swaminathan <114734554+arjhun-alt@users.noreply.github.com>
2025-11-14 14:59:40 +0100

    Update GWAS_lib.py

--- Code/GWAS_lib.py
+++ Code/GWAS_lib.py
@@ -8,11 +8,6 @@ from scipy.stats import chisquare
 
 np.random.seed(420)
 
-def get_O_X(C, seed):
-    A = np.random.rand(C + 1, C)
-    Q, R = np.linalg.qr(A)
-    return Q
-
 def load_X(N, M, C, P, B, p, b, idx, X_list):
     try:
         loaded_data = load_npz('../test_site/Data/N{}_M{}_C{}_P{}_B{}/Party_{}/X_block_{}.npz'.format(N, M, C, P, B, p, b))
@@ -28,34 +23,46 @@ def load_X(N, M, C, P, B, p, b, idx, X_list):
 def get_O(N, K, seed):
     def generate_semi_orthogonal_matrix(rows, cols):
         A = np.random.randn(rows, cols)
-        for i in range(cols):
-            for j in range(i):
-                A[:, i] -= np.dot(A[:, i], A[:, j]) / np.dot(A[:, j], A[:, j]) * A[:, j]
-            A[:, i] /= np.linalg.norm(A[:, i])
-        for i in range(rows):
-            for j in range(i + 1, cols):
-                dot_product = abs(np.dot(A[i, :], A[j, :]))
-                assert dot_product < 1, f"Dot product between row {i} and row {j} is {dot_product}, not less than 1"
+        for j in range(cols):
+            for i in range(j):
+                A[:, j] -= np.dot(A[:, j], A[:, i]) / np.dot(A[:, i], A[:, i]) * A[:, i]
+            norm = np.linalg.norm(A[:, j])
+            if norm != 0:
+                A[:, j] /= norm
         return A
+
     np.random.seed(seed)
     min_block_size = 95
     max_block_size = 100
+
+    if N <= max_block_size:
+        block = generate_semi_orthogonal_matrix(N + K, N)
+        O = csr_matrix(block)
+        O = random_permute_columns(O, 0)
+        return O
+
     num_blocks = N // max_block_size
-    while num_blocks<=K:
+    while num_blocks <= K:
         min_block_size -= 5
         max_block_size -= 5
+        if max_block_size <= 0:
+            block = generate_semi_orthogonal_matrix(N + K, N)
+            O = csr_matrix(block)
+            O = random_permute_columns(O, 0)
+            return O
         num_blocks = N // max_block_size
+
     remaining_columns = N % max_block_size
     blocks = []
     for i in range(K):
         block_size = np.random.randint(min_block_size, max_block_size + 1)
-        block = generate_semi_orthogonal_matrix(block_size+1,block_size)
+        block = generate_semi_orthogonal_matrix(block_size + 1, block_size)
         blocks.append(block)
     for i in range(num_blocks - 1 - K):
         block_size = np.random.randint(min_block_size, max_block_size + 1)
         block = generate_random_orthogonal_matrix(block_size)
         blocks.append(block)
-    last_block_size =max(N - sum(block.shape[1] for block in blocks),0)
+    last_block_size = max(N - sum(block.shape[1] for block in blocks), 0)
     last_block = generate_random_orthogonal_matrix(last_block_size)
     blocks.append(last_block)
     O_blocks = []
@@ -69,21 +76,30 @@ def get_O(N, K, seed):
         padded_block = hstack((left_padding, block, right_padding)).tocsc()
         padded_block = vstack((upper_padding, padded_block, lower_padding)).tocsc()
         O_blocks.append(padded_block)
-    O = random_permute_columns(sum(O_blocks),0)
+    O = random_permute_columns(sum(O_blocks), 0)
     return O
 
-def get_square_O(N, seed):
+def get_O_square(N, seed):
+    size = N
     np.random.seed(seed)
     min_block_size = 95
     max_block_size = 100
-    num_blocks = N // max_block_size
-    remaining_columns = N % max_block_size
+
+    if size <= max_block_size:
+        block = generate_random_orthogonal_matrix(size)
+        O = csr_matrix(block)
+        O = random_permute_square_columns(O, seed)
+        return O
+
+    num_blocks = size // max_block_size
+    remaining = size % max_block_size
+
     blocks = []
     for i in range(num_blocks):
-        if i != num_blocks - 1 or remaining_columns == 0:
+        if i != num_blocks - 1 or remaining == 0:
             block_size = max_block_size
         else:
-            block_size = remaining_columns
+            block_size = remaining
         block = generate_random_orthogonal_matrix(block_size)
         blocks.append(block)
 
@@ -107,7 +123,6 @@ def random_permute_square_columns(matrix, seed):
     permuted_matrix = matrix[:, permuted_indices]
     return permuted_matrix
 
-
 def random_permute_columns(matrix, seed):
     num_columns = matrix.shape[1]
     np.random.seed(seed)
@@ -123,55 +138,72 @@ def generate_random_orthogonal_matrix(size):
     Q[abs(Q) < tol] = 0
     return Q
 
-def standardization_normalizers(M, P, p, seed):
+def randomized_encoding_addtion(M, P, p, seed):
     np.random.seed(seed)
     matrix = np.random.randint(-10, 10, size=(P, M), dtype=np.int8)
     column_sum_vector = np.sum(matrix, axis=0, dtype=np.int32)
     return matrix[p - 1, :], column_sum_vector
 
-def generate_O_b(M, B, p, P, N, C, block_size,b):
-    if not os.path.exists('../test_site/Data/N{}_M{}_C{}_P{}_B{}/Masks'.format(N, M, C, P, B)):os.makedirs('../test_site/Data/N{}_M{}_C{}_P{}_B{}/Masks'.format(N, M, C, P, B),exist_ok=True)
+def generate_mask_beta_block(M, B, p, P, N, C, block_size, b):
+    directory = '../test_site/Data/N{}_M{}_C{}_P{}_B{}/Party_{}/Masks'.format(N, M, C, P, B, p)
+    if not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
     O = get_O(block_size, 0, b)
-    filename = os.path.join('../test_site/Data/N{}_M{}_C{}_P{}_B{}/Masks'.format(N, M, C, P, B),"O_b_block_{}.npz".format(b + 1))
+    filename = os.path.join(directory, "O_b_block_{}.npz".format(b + 1))
     save_npz(filename, O)
     del O
     gc.collect()
 
-def save_matrices(X,b,N,M,C,P,B):
-    if not os.path.exists('../test_site/Data/N{}_M{}_C{}_P{}_B{}/Server'.format(N, M, C, P, B)): os.makedirs('../test_site/Data/N{}_M{}_C{}_P{}_B{}/Server'.format(N, M, C, P, B), exist_ok=True)
-    np.save('../test_site/Data/N{}_M{}_C{}_P{}_B{}/Server/X_block_{}.npy'.format(N, M, C, P, B,b+1),X)
+def save_matrices(X, b, N, M, C, P, B):
+    if not os.path.exists('../test_site/Data/N{}_M{}_C{}_P{}_B{}/Server'.format(N, M, C, P, B)):
+        os.makedirs('../test_site/Data/N{}_M{}_C{}_P{}_B{}/Server'.format(N, M, C, P, B), exist_ok=True)
+    np.save('../test_site/Data/N{}_M{}_C{}_P{}_B{}/Server/X_block_{}.npy'.format(N, M, C, P, B, b + 1), X)
 
-def generate_O_X(M, B, p, P, N, C, block_size, additional_cols,b):
-    directory = '../test_site/Data/N{}_M{}_C{}_P{}_B{}/Masks'.format(N, M, C, P, B)
+
+def generate_mask_X_block(M, B, p, P, N, C, block_size, additional_cols, b):
+    directory = '../test_site/Data/N{}_M{}_C{}_P{}_B{}/Party_{}/Masks'.format(N, M, C, P, B, p)
     if not os.path.exists(directory):
         os.makedirs(directory, exist_ok=True)
-    if additional_cols<0:
-        print('additional_col = 0 found')
-        additional_cols=0
+    if additional_cols < 0:
+        additional_cols = 0
     O = get_O(block_size, additional_cols, b).tocsc()
     filename = os.path.join(directory, "O_X_block_{}.npz".format(b + 1))
     save_npz(filename, O)
     del O
     gc.collect()
 
-def generate_Z_mask(M, B, p, P, N, C):
-    directory = '../test_site/Data/N{}_M{}_C{}_P{}_B{}/Masks'.format(N, M, C, P, B)
+def generate_O_Z(M, B, p, P, N, C):
+    directory = '../test_site/Data/N{}_M{}_C{}_P{}_B{}/Party_{}/Masks'.format(N, M, C, P, B, p)
     if not os.path.exists(directory):
         os.makedirs(directory, exist_ok=True)
-    if p == 1:
-        O = get_O(N, 1, 0)
-        save_npz(
-            '../test_site/Data/N{}_M{}_C{}_P{}_B{}/Masks/Z_mask.npz'.format(N, M, C, P, B),O)
-        del O
-        gc.collect()
+    O = get_O(N, 1, 0)
+    save_npz(
+        '../test_site/Data/N{}_M{}_C{}_P{}_B{}/Party_{}/Masks/O_Z.npz'.format(N, M, C, P, B, p), O)
+    del O
+    gc.collect()
 
-def generate_O(M, B, K, p, P, N, C):
-    if p == 1:
-        O = get_O(N, 1, 1)
-        save_npz(
-            '../test_site/Data/N{}_M{}_C{}_P{}_B{}/Masks/O.npz'.format(N, M, C, P, B),O)
-        del O
-        gc.collect()
+
+def generate_O_Z_prime(M, B, p, P, N, C):
+    directory = '../test_site/Data/N{}_M{}_C{}_P{}_B{}/Party_{}/Masks'.format(N, M, C, P, B, p)
+    if not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
+    O = get_O_square(C+1, 0)
+    save_npz(
+        '../test_site/Data/N{}_M{}_C{}_P{}_B{}/Party_{}/Masks/O_Z_prime.npz'.format(N, M, C, P, B, p), O)
+    del O
+    gc.collect()
+
+
+
+def generate_O_y_tilde(M, B, K, p, P, N, C):
+    directory = '../test_site/Data/N{}_M{}_C{}_P{}_B{}/Party_{}/Masks'.format(N, M, C, P, B, p)
+    if not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
+    O = get_O(N, 1, 1)
+    save_npz(
+        '../test_site/Data/N{}_M{}_C{}_P{}_B{}/Party_{}/Masks/O_y_tilde.npz'.format(N, M, C, P, B, p), O)
+    del O
+    gc.collect()
 
 def generate_a_number(seed):
     random.seed(seed)
